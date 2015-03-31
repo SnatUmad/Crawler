c0c0052d9ea62c08137154b8d4f09235929d8ae8
SnatUmad <adajio.il34@gmail.com>
2015-04-01 01:07:40 +0300

    еще тести

--- src/parcer/ParserGUI.java
+++ src/parcer/ParserGUI.java
@@ -53,7 +53,7 @@ public class ParserGUI {
 		frame.getContentPane().setLayout(null);
 
 		txtHttp = new JTextField();
-		txtHttp.setText("http://");
+		txtHttp.setText("http://lolmail.ru");
 		txtHttp.setBounds(104, 539, 289, 20);
 		frame.getContentPane().add(txtHttp);
 		txtHttp.setColumns(10);
